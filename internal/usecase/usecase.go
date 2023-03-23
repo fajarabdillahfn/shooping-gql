40ff88bfa22da0f6f33e188efa2e21be50816a51
fajar-abdillahfn <abdillah.fajar@tokopedia.com>
2023-03-23 14:35:59 +0700

    feat: add usecase and checkout method

--- internal/usecase/usecase.go
+++ internal/usecase/usecase.go
@@ -1,3 +1,11 @@
 package usecase
 
-type UseCase interface{}
+import (
+	"context"
+
+	"github.com/fajarabdillahfn/shoping-gql/internal/model"
+)
+
+type UseCase interface {
+	Checkout(ctx context.Context, productsBought map[string]int) (*model.Cart, error)
+}
---
+++ internal/usecase/v1/checkout.go
@@ -0,0 +1,118 @@
+package v1
+
+import (
+	"context"
+	"fmt"
+	"math"
+	"time"
+
+	"github.com/fajarabdillahfn/shoping-gql/internal/model"
+)
+
+func (u *useCase) Checkout(ctx context.Context, productsBought map[string]int) (*model.Cart, error) {
+	ctx, cancel := context.WithTimeout(ctx, time.Millisecond*2000)
+	defer cancel()
+
+	var (
+		googleHomeSku = "120P90"
+		alexaSku      = "A304SD"
+		macBookSku    = "43N23P"
+		rasPiSku      = "234234"
+		isPromotion3  = false
+	)
+
+	cart := model.Cart{
+		TotalPrice: 0,
+	}
+
+	for sku, qty := range productsBought {
+		ctxSku := context.WithValue(ctx, "sku", sku)
+		product, err := u.ShopRepo.GetBySku(ctxSku)
+		if err != nil {
+			return nil, err
+		}
+
+		if product.Quantity < qty {
+			return nil, fmt.Errorf("INVALID QUANTITY: we don't have enough %s product", product.Name)
+		}
+
+		checkoutProduct := model.CheckoutProduct{
+			Sku:      product.Sku,
+			Name:     product.Name,
+			Price:    product.Price,
+			Quantity: qty,
+		}
+
+		switch {
+		// PROMOTION 1: Each sale of a MacBook Pro comes with a free Raspberry Pi B
+		case sku == macBookSku:
+			isPromotion3 = true
+			checkoutProduct.TotalPrice = product.Price * float64(qty)
+
+		// PROMOTION 2: Buy 3 Google Homes for the price of 2
+		case sku == googleHomeSku && qty >= 3:
+			disc := math.Floor(3 / float64(qty))
+			checkoutProduct.TotalPrice = product.Price * (float64(qty) - disc)
+
+		// PROMOTION 3: Buying more than 3 Alexa Speakers will have a 10% discount on all Alexa speakers
+		case sku == alexaSku && qty > 3:
+			checkoutProduct.TotalPrice = (product.Price * float64(qty)) * 0.9
+
+		default:
+			checkoutProduct.TotalPrice = product.Price * float64(qty)
+		}
+
+		cart.Products = append(cart.Products, &checkoutProduct)
+		cart.TotalPrice += checkoutProduct.TotalPrice
+	}
+
+	// PROMOTION 1: Each sale of a MacBook Pro comes with a free Raspberry Pi B
+	if isPromotion3 {
+		qtyRasPi, buyRasPi := productsBought[rasPiSku]
+		qtyMacBook, _ := productsBought[macBookSku]
+
+		var newQtyRasPi int
+
+		ctxSku := context.WithValue(ctx, "sku", rasPiSku)
+		rasPi, err := u.ShopRepo.GetBySku(ctxSku)
+		if err != nil {
+			return nil, err
+		}
+
+		if qtyMacBook > qtyRasPi {
+			if rasPi.Quantity > qtyMacBook {
+				newQtyRasPi = qtyMacBook
+			} else {
+				newQtyRasPi = rasPi.Quantity
+			}
+		} else {
+			newQtyRasPi = qtyRasPi
+		}
+
+		if buyRasPi {
+			for i, productCart := range cart.Products {
+				if productCart.Sku == rasPiSku {
+					cart.Products[i].Quantity = newQtyRasPi
+					cart.Products[i].TotalPrice = float64(newQtyRasPi - qtyMacBook) * rasPi.Price
+					break
+				}
+			}
+		} else {
+			rasPiProduct := model.CheckoutProduct{
+				Sku:      rasPi.Sku,
+				Name:     rasPi.Name,
+				Price:    rasPi.Price,
+				Quantity: newQtyRasPi,
+				TotalPrice: float64(newQtyRasPi - qtyMacBook) * rasPi.Price,
+			}
+			
+			cart.Products = append(cart.Products,&rasPiProduct)
+
+			cart.TotalPrice += rasPiProduct.TotalPrice
+		}
+
+		cart.TotalPrice -= float64(qtyMacBook) * rasPi.Price
+	}
+
+	return &cart, nil
+}
