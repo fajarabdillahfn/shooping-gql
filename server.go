88a3cf7c7f40210376675acc258a9fd97ddd8de3
fajar-abdillahfn <abdillah.fajar@tokopedia.com>
2023-03-24 15:38:11 +0700

    feat: update log

--- server.go
+++ server.go
@@ -25,6 +25,6 @@ func main() {
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Printf("start server on port :%s", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
